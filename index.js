771e1114c34cc4248dcfc55e5bb475d7acacaff9
Grzegorz Kielar <grzegorz.kielar@kielniakodu.io>
2023-07-06 14:00:00 +0200

    Add merge sort

--- index.js
+++ index.js
@@ -47,6 +47,39 @@ function bubbleSort(arr) {
 const array3 = [5, 2, 4, 6, 1, 3];
 console.log(bubbleSort(array3));
 
+function mergeSort(arr) {
+	if (arr.length <= 1) {
+		return arr;
+	}
+
+	const middle = Math.floor(arr.length / 2);
+	const left = arr.slice(0, middle);
+	const right = arr.slice(middle);
+
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+	const result = [];
+	let leftIndex = 0;
+	let rightIndex = 0;
+
+	while (leftIndex < left.length && rightIndex < right.length) {
+		if (left[leftIndex] < right[rightIndex]) {
+			result.push(left[leftIndex]);
+			leftIndex++;
+		} else {
+			result.push(right[rightIndex]);
+			rightIndex++;
+		}
+	}
+
+	return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const array5 = [5, 2, 4, 6, 1, 3];
+console.log(mergeSort(array5));
+
 function quickSort(arr) {
 	if (arr.length <= 1) {
 		return arr;

3b29bdc2b802f2c0722a15e00db63bfd607333dd
Grzegorz Kielar <grzegorz.kielar@kielniakodu.io>
2023-07-06 13:48:50 +0200

    Add Bubble sort

---
+++ index.js
@@ -0,0 +1,48 @@
+function insertionSort(arr) {
+	for (let i = 1; i < arr.length; i++) {
+		let currentValue = arr[i];
+		let j = i - 1;
+		while (j >= 0 && arr[j] > currentValue) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = currentValue;
+	}
+	return arr;
+}
+
+const array = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(array));
+
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
+		if (minIndex !== i) {
+			[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+		}
+	}
+	return arr;
+}
+
+const array2 = [5, 2, 4, 6, 1, 3];
+console.log(selectionSort(array2));
+
+function bubbleSort(arr) {
+	const length = arr.length;
+	for (let i = 0; i < length - 1; i++) {
+		for (let j = 0; j < length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+			}
+		}
+	}
+	return arr;
+}
+
+const array3 = [5, 2, 4, 6, 1, 3];
+console.log(bubbleSort(array3));
